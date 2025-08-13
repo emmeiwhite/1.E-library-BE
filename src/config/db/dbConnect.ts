de828ab823608c5bee38dffd40163cebdddd5614
emmeiwhite <emmeiwhite@gmail.com>
2025-08-14 02:28:37 +0530

    12. Setup event handlers while connecting to DB

--- src/config/db/dbConnect.ts
+++ src/config/db/dbConnect.ts
@@ -2,5 +2,22 @@ import mongoose from 'mongoose'
 import { configs } from '../_config'
 
 async function connectDB() {
-  await mongoose.connect(configs.MONGO_URI as string)
+  try {
+    mongoose.connection.on('connected', () => {
+      console.log('Connected to DB successfully!')
+    })
+
+    //   In future if we get any error
+    mongoose.connection.on('error', () => {
+      console.log('Error in connecting to the database')
+    })
+    await mongoose.connect(configs.MONGO_URI as string)
+  } catch (error) {
+    console.log('Failed to establish database connection', error)
+
+    //   Stop the application (server), if we fail to connect to the databse
+    process.exit(1)
+  }
 }
+
+export default connectDB
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import app from './app'
 import { configs } from './config/_config'
+import connectDB from './config/db/dbConnect'
 
 const PORT = configs.PORT || 3000
-
+connectDB()
 function start() {
   app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`)
