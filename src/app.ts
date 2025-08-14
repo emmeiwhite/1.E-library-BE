2b4d6388f4d79ad822b0ab740b5ba34754646a76
emmeiwhite <emmeiwhite@gmail.com>
2025-08-14 10:58:27 +0530

    16. Global Error Handler --- Sending errorStack wisely only in the development mode

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express, { NextFunction, Request, Response } from 'express'
+import { HttpError } from 'http-errors'
+import { configs } from './config/_config'
 
 const app = express()
 
 // Global Error Handler
-app.use((err, req: Request, res: Response, next: NextFunction) => {
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.statusCode || 500
+
+  res.status(statusCode).json({
+    message: err.message,
+    errorStack: configs.NODE_ENV === 'development' ? err.stack : ''
+  })
 })
 
 export default app
--- src/config/_config.ts
+++ src/config/_config.ts
@@ -3,7 +3,8 @@ dotenv.config()
 
 const _config = {
   PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  NODE_ENV: process.env.NODE_ENV
 }
 
 export const configs = Object.freeze(_config)
