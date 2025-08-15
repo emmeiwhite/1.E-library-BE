a8bf37f261dd6b7a46c9a23b37736fd76c096b13
emmeiwhite <emmeiwhite@gmail.com>
2025-08-15 23:56:47 +0530

    27. Check whether exists in the database

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,23 @@
 import { Response, Request, NextFunction } from 'express'
 import createHttpError from 'http-errors'
+import User from '../models/User'
 
 // 1. Create User
-export const createUser = (req: Request, res: Response, next: NextFunction) => {
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // 1. Validate the user & Sanitize
-
   const { name, email, password } = req.body
 
   if (!name || !email || !password) {
     const error = createHttpError(400, 'All fields are required ')
+    return next(error)
+  }
+
+  // 2. Check whether the email exist in the database
+
+  const user = await User.findOne({ email })
+
+  if (user) {
+    const error = createHttpError(400, 'User already exist with this email ')
     return next(error)
   }
 
