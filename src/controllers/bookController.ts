eaca89e1bac8241a7b9ae61403c1b1159abab600
emmeiwhite <emmeiwhite@gmail.com>
2025-08-29 23:08:03 +0530

    46. Install cloudinary to upload images

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -2,4 +2,7 @@ import { Response, Request, NextFunction } from 'express'
 import createHttpError from 'http-errors'
 
 // We'll need to perform all the CRUD Operations on Book itself
-export const createBook = async (req: Request, res: Response, next: NextFunction) => {}
+export const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.files)
+  res.send({ message: 'Testing' })
+}
